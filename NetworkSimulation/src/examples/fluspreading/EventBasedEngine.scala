007f3445de4b788171d6a62ed0717ca514509fc7
Jordi Tielens <jordi.tielens@student.kuleuven.com>
2011-11-10 10:11:18 +0100

    Event queue changed

--- NetworkSimulation/src/examples/fluspreading/EventBasedEngine.scala
+++ NetworkSimulation/src/examples/fluspreading/EventBasedEngine.scala
@@ -1,23 +1,23 @@
 package examples.fluspreading
 
 import graph.Graph
-import scala.collection.mutable.Queue
 import com.sun.xml.internal.ws.api.pipe.Engine
+import scala.util.Random
+import scala.collection.mutable.PriorityQueue
 
 class EventBasedEngine(graph:Graph) extends SimulationEngine(graph) {
 
-  var events:Queue[Event] = new Queue[Event]
+  var events:PriorityQueue[Event] = new PriorityQueue[Event]()
   var clients:List[EventClient] = List[EventClient]()
   
   
   def run(){
-    while(shouldContinue) {
-      if(!events.isEmpty) {
-        events.foreach(event => 
+    events.enqueue(new SimulationStarted)
+        while(shouldContinue && !events.isEmpty) {
+          var event = events.dequeue() 
+          currentTime = event.timestamp
           clients.foreach(client=> client.notify(event) )
-        )
-      }
-    }
+        }
   }
   
   def addEventClient(client:EventClient) {clients ::= client}
@@ -27,7 +27,11 @@ class EventBasedEngine(graph:Graph) extends SimulationEngine(graph) {
 
 abstract class Event {
   var timestamp:Int = 0
+  
+  
 }
+
+class SimulationStarted extends Event{}
 
 trait EventClient {
   private var engine:EventBasedEngine = null
@@ -35,7 +39,7 @@ trait EventClient {
     engine = e
   }
   def createEvent(event:Event){
-    event.timestamp = engine.getCurrentTime
+    event.timestamp = (engine.getCurrentTime + Random.nextInt(100))
     engine.send(event)
   }
   def notify(event:Event)
