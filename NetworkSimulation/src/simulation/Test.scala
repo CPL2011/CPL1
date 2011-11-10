0f739f5293732313d18a63bde8731df6bf2bf618
DriesVanDaele <dries_van_daele@telenet.be>
2011-11-10 19:57:07 +0100

    accidentally commented out too much code, PS ik heb nog niet al mn code kunnen pushen. Scala wil de code weer niet compileren. Krijg een NoClassDefFoundError. kProbeer het op te lossen, maar ongeteste code push ik liever niet, dus ff geduld aub ;) meeste verdere aanpassing zijn intern vermoed ik dus de interface die je hanteert zou niet sterk mogen veranderen.

--- NetworkSimulation/src/simulation/Test.scala
+++ NetworkSimulation/src/simulation/Test.scala
@@ -27,70 +27,70 @@ object Test extends Application {
 //  
 //  
 //  
-//  //var test = new TestDescription(true, "http://192.168.56.101:20738/RPC2")
-//  //var test = new TestDescription(true,"http://192.168.253.128:20738/RPC2")
-//  var test = new TestDescription(false,"")
-//  var graph = new testGraph()
-//  graph.addStatistic(graph.numberOfNodes,"NoN")
-//  graph.addStatistic(graph.averageNeighbores,"AvEdges")
-//
-//  var i = 1
-//  while(i<=21) {
-//    graph.addNode(i)
-//    if(i%5==0)graph.gatherStat(graph)
-//    i+=1
-//  }
-//  
-//  graph.addEdge(1, 2)
-//  graph.addEdge(1, 3)
-//  graph.addEdge(2, 4)
-//  graph.addEdge(2, 5)  
-//  graph.addEdge(3, 6)
-//  graph.gatherStat(graph)
-//  graph.addEdge(3, 7)
-//  graph.addEdge(4, 8)
-//  graph.addEdge(8, 9)
-//  graph.addEdge(5, 6)
-//  graph.gatherStat(graph)
-//  graph.addEdge(10, 11)
-//  graph.addEdge(12, 13)
-//  graph.addEdge(6, 14)
-//  graph.addEdge(6, 15)
-//  graph.addEdge(15, 16)
-//  graph.gatherStat(graph)
-//  graph.addEdge(15, 17)
-//  graph.addEdge(15, 18)
-//  graph.addEdge(1, 19)
-//  graph.addEdge(1, 20)
-//  graph.addEdge(1, 21)
-//  graph.gatherStat(graph)
-//  
-//  graph.writeStatisticsToFile("test.txt")
-//  
-//  //print the object ids of the reachable nodes of each node it visits (in order)
-//  graph.traverse(BreadthFirstTraversal, e => println(e.originatingEdges.toString), 1) 
-//  //check if two nodes are connected
-//  var connected = false
-//  graph.traverse(BreadthFirstTraversal, node => if (node.label == 9) connected = true, 2) 
-//  if (connected) System.out.println("connected") else System.out.println("unconnected") 
-//
-//  //retrieve an ordered list of the visited nodes 
-//  var nodeListBuffer = new ListBuffer[Node]()
-//  graph.traverse(BreadthFirstTraversal, node => nodeListBuffer += node, 1)
-//  System.out.println("The ListBuffer contains an ordered list of size " + nodeListBuffer.size)
-//
-//  graph.traverse(DepthFirstTraversal, e => println(e.originatingEdges.toString), 100) // should trigger an error message
-//  graph.removeEdge(1, 2)
-//  graph.removeEdge(1, 3)
-//  graph.removeEdge(1, 21)
-////  graph.removeEdge(1, 20)
-////  graph.removeEdge(1, 19)
-////  graph.removeNode(1)
-//
+  //var test = new TestDescription(true, "http://192.168.56.101:20738/RPC2")
+  //var test = new TestDescription(true,"http://192.168.253.128:20738/RPC2")
+  var test = new TestDescription(false,"")
+  var graph = new testGraph()
+  graph.addStatistic(graph.numberOfNodes,"NoN")
+  graph.addStatistic(graph.averageNeighbores,"AvEdges")
+
+  var i = 1
+  while(i<=21) {
+    graph.addNode(i)
+    if(i%5==0)graph.gatherStat(graph)
+    i+=1
+  }
+  
+  graph.addEdge(1, 2)
+  graph.addEdge(1, 3)
+  graph.addEdge(2, 4)
+  graph.addEdge(2, 5)  
+  graph.addEdge(3, 6)
+  graph.gatherStat(graph)
+  graph.addEdge(3, 7)
+  graph.addEdge(4, 8)
+  graph.addEdge(8, 9)
+  graph.addEdge(5, 6)
+  graph.gatherStat(graph)
+  graph.addEdge(10, 11)
+  graph.addEdge(12, 13)
+  graph.addEdge(6, 14)
+  graph.addEdge(6, 15)
+  graph.addEdge(15, 16)
+  graph.gatherStat(graph)
+  graph.addEdge(15, 17)
+  graph.addEdge(15, 18)
+  graph.addEdge(1, 19)
+  graph.addEdge(1, 20)
+  graph.addEdge(1, 21)
+  graph.gatherStat(graph)
+  
+  graph.writeStatisticsToFile("test.txt")
+  
+  //print the object ids of the reachable nodes of each node it visits (in order)
+  graph.traverse(BreadthFirstTraversal, e => println(e.originatingEdges.toString), 1) 
+  //check if two nodes are connected
+  var connected = false
+  graph.traverse(BreadthFirstTraversal, node => if (node.label == 9) connected = true, 2) 
+  if (connected) System.out.println("connected") else System.out.println("unconnected") 
+
+  //retrieve an ordered list of the visited nodes 
+  var nodeListBuffer = new ListBuffer[Node]()
+  graph.traverse(BreadthFirstTraversal, node => nodeListBuffer += node, 1)
+  System.out.println("The ListBuffer contains an ordered list of size " + nodeListBuffer.size)
+
+  //graph.traverse(DepthFirstTraversal, e => println(e.originatingEdges.toString), 100) // should trigger an error message
+  //graph.removeEdge(1, 2)
+  //graph.removeEdge(1, 3)
+  //graph.removeEdge(1, 21)
+//  graph.removeEdge(1, 20)
+//  graph.removeEdge(1, 19)
+//  graph.removeNode(1)
+
 //  graph.visualize
 //  Thread.sleep(5000)
 //  graph.removeVisualization
-//  
+  
 //  
 //  System.out.println("---------------------------")
 //  
