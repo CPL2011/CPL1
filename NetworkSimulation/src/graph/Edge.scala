7c743a3ddaaf1202dd7aa6819ed6498a928c8641
Andreas Put <andreasput@gmail.com>
2011-11-17 01:02:47 +0100

    mooore documentation-_-

--- NetworkSimulation/src/graph/Edge.scala
+++ NetworkSimulation/src/graph/Edge.scala
@@ -1,8 +1,21 @@
 package graph
 import org.ubiety.ubigraph.UbigraphClient
-
+/**
+  * An edge symbolizes a connection between two Nodes
+  * Default edges are directed edges
+  * 
+  * @param source The Node where the edge starts
+  * @param destination The NOde where the edge ends  
+  */
 class Edge(val source: Node, val destination: Node) {
   
+  /**
+    * After an edge has been created, the nodes of the edge needs to be informed 
+    * that there is an edge between them.
+    * Only if this method is invoked will the Graph 'know' that there is an edge between these nodes
+    * 
+    *  @param Graph The graph to which the nodes of the edge belong.
+    */
   def informNodes(graph: Graph) = {
     if (graph.hasNode(source) && graph.hasNode(destination)) { 
       source.addOriginatingEdge(this)
--- NetworkSimulation/src/graph/Node.scala
+++ NetworkSimulation/src/graph/Node.scala
@@ -5,48 +5,89 @@ import engine.Event
 import engine.TurnClient
 import engine.EventClient
 import engine.RoundClient
-
+/**
+  * A node represents an actor in a network 
+  * 
+  * @param label A unique integer that corresponds to this Node
+  */
 class Node(val label: Int) extends TurnClient with RoundClient with EventClient {
-  
+  /**
+    * A Map that contains the keys and edges that start in this node
+    * The key is a unique integer
+    */
   var originatingEdges = new HashMap[Int, Edge]()
+    /**
+    * A Map that contains the keys and edges that end in this node
+    * The key is a unique integer
+    */
   var arrivingEdges = new HashMap[Int, Edge]()
   
+  /** Get an edge that starts in this node that corresponds to the given key
+    * @param destination the key is used to retrieve an edge
+    * @return An Option[Edge] corresponding to the given key
+    */
   def getEdgeTo(destination : Int) : Option[Edge] = originatingEdges.get(destination)
   
+  /** Get an edge that arrives in this node that corresponds to the given key
+    * @param source  the key is used to retrieve an edge
+    * @return An Option[Edge] corresponding to the given key
+    */
   def getEdgeFrom(source : Int) : Option[Edge] = arrivingEdges.get(source)
-  
+  /**
+    *Get the nodes at the other end of the edges that end in this node
+    *@return a List containing the parents of this node 
+    */
   def getParents : List[Node] = {
     var parents : List[Node] = Nil
     arrivingEdges.values.foreach(e => parents ::= e.source)
     parents
   }
-  
+  /**
+    * get the nodes at the other end of the edges that start in this node
+    * @return a List containing the children of this node
+    */
   def getChildren : List[Node] = {
     var children : List[Node] = Nil
     originatingEdges.values.foreach(e => children ::= e.destination)
     children
   }
-  
+  /**
+    * Get the nodes at the other end of the edges that are connected to this nodes
+    * @return a List containing the neighbours of this node
+    */
   def getNeighbours : List[Node] = getParents ++ getChildren
-
+  /**
+    * Add an outgoing edge by adding it to the originatingEdges map
+    * @param edge the edge that is added to the node
+    */
   def addOriginatingEdge(edge: Edge) = { 
     if (!originatingEdges.contains((edge.destination.label))) {
       originatingEdges += ((edge.destination.label, edge))
     } else System.err.println("The edge (" + edge.source.label + "," + edge.destination.label + ") has already been added to this graph")
   }
-  
+  /**
+    * Add an incoming edge by adding it to the arrivingEdges map
+    * @param edge the edge that is added to the node
+    */
   def addArrivingEdge(edge: Edge) = { 
     if (!arrivingEdges.contains((edge.source.label))) {
       arrivingEdges += ((edge.source.label, edge))
     } else System.err.println("The edge (" + edge.source.label + "," + edge.destination.label + ") has already been added to this graph")
   }
   
+  /**
+    * Add a 'bidirectional' edge by adding it to the arrivingEdges and originatingEdges maps
+    * @param edge the edge that is added to the node
+    */
   def addNeighbour(node : Node) = {
     var edge = new Edge(this, node)
     originatingEdges += ((node.label, edge))
     node.arrivingEdges += ((label, edge))
   }
-  
+  /**
+    * removes a 'bidirectional' edge by removing it from the arrivingEdges and originatingEdges maps
+    * @param nodeID the node identifier of the node to which the edge lies that will be removed
+    */
   def removeNeighbour(nodeID: Int) = {
     originatingEdges.get(nodeID) match {
       case Some(edge) => {
@@ -59,6 +100,9 @@ class Node(val label: Int) extends TurnClient with RoundClient with EventClient 
     } 
   }
   
+  /** Removes the edges to and from this node.
+    *  Before the node can be removed from the graph, all edges to and from this node need to be removed.
+    */
   def remove = {
     arrivingEdges.values.foreach(e => e.source.removeNeighbour(label))
     //after this run arrivingEdges should be empty
@@ -71,62 +115,4 @@ class Node(val label: Int) extends TurnClient with RoundClient with EventClient 
   def nextRound {}
   def notify(event:Event){}
   
-//  def visualize(ubigraphClient : UbigraphClient) = {
-//    if (ubigraphClient.newVertex(label) == -1) {
-//      System.err.println("Node " + label + " has already been visualized")
-//    }
-//  }
-  
-//  def removeVisualization(ubigraphClient : UbigraphClient) = {
-//    if (ubigraphClient.removeVertex(label) == -1) {
-//      System.err.println("Node " + label + " has not yet been visualized")
-//    }
-//  }
-  
- /// def toXML() = <Node label={label.toString()}>
-  //    {originatingEdges.map(e=>e.toXML())}
-  //  </Node>
-  
-  
-}
-
-trait Infectable{
-  var infected:Boolean = false
-  var dead:Boolean = false
-
-  def isInfected(infectedNeighbours:Int,infectionChance:Float):Boolean = {
-    if(dead) false
-    else if(infected) true
-    else{
-      if(Random.nextFloat()> Math.pow(1-infectionChance,infectedNeighbours)){// if randomnumber > survivalchance(==1-infectionchance)^infectedneighbours
-    	  infect()	
-    	  true											// infection will take place
-      }
-      else false
-     }
-  }
-  
-  private def infect(){
-    infected=true
-  }
-}
-
-class InfectableNode(label:Int, var infectionChance:Float) extends Node(label) with Infectable{
-  def this(n:scala.xml.Node) = {
-    this((n\"@label").text.toInt,(n\"infectionChance").text.toFloat)
-    dead = (n\"dead").text.toBoolean
-    infected = (n\"infected").text.toBoolean
-  }
-  
-  def infect(){
-    isInfected(originatingEdges.size, infectionChance)
-  }
-  
-  
-  //override def toXML() = <Node label={label.toString()}>
-  //    {originatingEdges.map(e=>e.toXML())}
-  //    <infected>{infected}</infected>
-  //    <infectionChance>{iChance}</infectionChance>
-  //    <dead>{dead}</dead>
-  //  </Node>
 }
