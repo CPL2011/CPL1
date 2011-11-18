eae90f68380f0782afc2b116f76203711faa961c
Van Daele D <dandonio@hotmail.com>
2011-11-18 13:28:22 +0100

    Minor change to the code of examples.persistence.Db4oGraph Also wrote docs for said class as well as examples.fluspreading.Visualgraph

--- NetworkSimulation/src/examples/fluspreading/VisualGraph.scala
+++ NetworkSimulation/src/examples/fluspreading/VisualGraph.scala
@@ -7,18 +7,36 @@ import engine.SimulationTime
 import graph.persistence.Db4oPersistence
 import examples.persistence.Db4oGraph
 
+/**
+ * A graph class that supports loading a graph from storage as well as the functionality
+ * to give a visual representation of itself
+ */
 class VisualGraph(p:String) extends Db4oGraph(p) with Visualizable with Db4oPersistence{
   val REFRESH_RATE = 2 * SimulationTime.TICKS_PER_MINUTE
   var elapsedTime = REFRESH_RATE + 1
   var refreshRate:Int = REFRESH_RATE
   
+  /**
+   * Loads a graph object from the path given by the class parameter p.
+   * If no graph is present, null gets returned
+   */
   def loadGraph():VisualGraph = load() match{
     case Some(t:VisualGraph)=>t
     case _=>null
   }
   
+  /**
+   * sets the refresh rate at the given Integer-value.
+   */
   def setRefreshRate(rr:Int) {refreshRate = rr}
   
+  /**
+   * Move the elapsed time forward with the given increment.
+   * If the elapsed time exceeds the period between each refresh,
+   * the graph gets re-visualized.
+   * @param duration : The period of time by which the visualization should be
+   * advanced.
+   */
   def refreshVisualization(duration:Int){
     if(elapsedTime > REFRESH_RATE) {
       visualize(updateVisualization)
@@ -27,6 +45,11 @@ class VisualGraph(p:String) extends Db4oGraph(p) with Visualizable with Db4oPers
     elapsedTime += duration
   }
   
+  /**
+   * If the given node is an instance of the Person class,
+   * its visualization gets updated.
+   * @param n : a instance of class Node, or of a class extending Node
+   */
   def updateVisualization(n:Node){
     n match{
       case p:Person => p.updateVisualization(ubigraphClient)
@@ -34,6 +57,10 @@ class VisualGraph(p:String) extends Db4oGraph(p) with Visualizable with Db4oPers
     }
   }
   
+  /**
+   * A def overridden from Visualizable, which will apply the def updateVisualization
+   * on each node when the graph gets traversed during visualization. 
+   */
   override def visualize {
     visualize(updateVisualization)
   }
--- NetworkSimulation/src/examples/persistence/Db4oGraph.scala
+++ NetworkSimulation/src/examples/persistence/Db4oGraph.scala
@@ -3,13 +3,23 @@ package examples.persistence
 import graph.persistence.Db4oPersistence
 import graph.Graph
 
+/**
+ * A graph that supports loading a graph from storage
+ */
 class Db4oGraph(p:String) extends Graph with Db4oPersistence{
+  /**
+   * Stores the given class parameter p under the var path
+   */
   override var path = p
   
+  /**
+   * Loads the graph present in the given path
+   * If the stored graph is not an instance of class Db4oGraph then None gets returned
+   */
   override def load():Option[Db4oGraph] = {
     super.load() match{
-      case t:Option[Db4oGraph] => t
-      case _ => null
+      case t:Some[Db4oGraph] => t
+      case _ => None
     }
    
   }
